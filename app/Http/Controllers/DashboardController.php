2666b36b5b0da1744fd377c85fba532324a4b936
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2022-12-08 14:35:37 +0700

    hardcode sementara sop

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Menu;
 use App\Models\quote;
+use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $response = file_get_contents(config('setting.api_url') . '/api/v1/sopid?start=1&limit=5');
+        $response = file_get_contents(url('/json/sopid.json'));
         $rules = json_decode($response)->results;
         $menu = new Menu();
 
